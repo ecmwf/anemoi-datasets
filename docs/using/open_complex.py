f5fb5be79b863bc3c3b5c8a571f704953ff41c63
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2024-03-28 14:54:12 +0000

    more doc

--- docs/using/open_complex.py
+++ docs/using/open_complex.py
@@ -4,8 +4,8 @@ config = {
     "dataset": {
         "ensemble": [
             "/path/to/dataset1.zarr",
-            {"dataset": "dataset_name", "end": "2010"},
-            {"dataset": "s3://path/to/dataset3.zarr", "begin": "2000", "end": "2010"},
+            {"dataset": "dataset_name", "end": 2010},
+            {"dataset": "s3://path/to/dataset3.zarr", "begin": 2000, "end": 2010},
         ],
         "frequency": "24h",
     },
