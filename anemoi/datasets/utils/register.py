d69e44f7ac989e4198cc1a44ebe1f201e1015b05
Florian Pinault <Florian.Pinault@ecmwf.int>
2024-04-11 15:45:41 +0000

    adding registering to the dataset catalogue

---
+++ anemoi/datasets/utils/register.py
@@ -0,0 +1,166 @@
+import logging
+from copy import deepcopy
+
+from anemoi.datasets import open_dataset
+
+LOG = logging.getLogger(__name__)
+
+
+def _lookup(top, paths, default):
+    if not isinstance(paths, (list, tuple)):
+        paths = [paths]
+    for path in paths:
+        obj = top
+        for p in path.split("."):
+            obj = obj.get(p)
+            if obj is None:
+                break
+        if obj is not None:
+            return obj
+    return default
+
+
+def simple_mapping(record, mapping, defaults={}):
+    display = dict(**record.get("display", {}))
+    for k, v in mapping.items():
+        display[k] = _lookup(record, v, defaults.get(k, "-"))
+    return display
+
+
+def to_by(values):
+    try:
+        diff = int(values[1]) - int(values[0])
+        for i, s in enumerate(values[2:]):
+            if int(values[i + 2]) - int(values[i + 1]) != diff:
+                return values
+
+    except ValueError:
+        return values
+
+    if diff == 1:
+        return [" ".join(str(x) for x in [values[0], "to", values[-1]])]
+    else:
+        return [" ".join(str(x) for x in [values[0], "to", values[-1], "by", diff])]
+
+
+def build_forcing_and_variables(record):
+    metadata = record["metadata"]
+    param_level = metadata["data_request"]["param_level"]
+    display = {}
+    seen = set()
+    for levtype, values in param_level.items():
+        display[levtype] = []
+        levels = []
+        params = []
+        for value in values:
+            if isinstance(value, list):
+                params.append(value[0])
+                levels.append(value[1])
+                value = "_".join(str(x) for x in value)
+                seen.add(value)
+            else:
+                value = str(value)
+                seen.add(value)
+                display[levtype].append(value)
+
+        if levels:
+            levels = to_by(sorted(set(levels)))
+            levels = ", ".join(str(x) for x in levels)
+            params = ", ".join(str(x) for x in sorted(set(params)))
+            display[levtype] = f"{params} on levels: {levels}"
+        else:
+            display[levtype] = ", ".join(display[levtype])
+
+    variables = sorted(set(metadata["variables"]))
+    forcings = []
+
+    # Keep the order
+
+    for v in variables:
+        if v not in seen:
+            forcings.append(v)
+    display["forcings"] = ", ".join(sorted(forcings))
+    return display
+
+
+def create_catalogue_record(path):
+    import os
+
+    import zarr
+
+    z = zarr.open(path)
+    ds = open_dataset(path)
+    name, _ = os.path.splitext(os.path.basename(path))
+
+    record = {
+        "name": name,
+        "uuid": z.attrs.get("uuid"),
+        "metadata": z.attrs.asdict(),
+        "display": {},
+    }
+
+    display = record["display"]
+
+    display["shape"] = z.data.shape
+
+    mapping = {
+        "name": "name",
+        "description": "metadata.description",
+        "format": "metadata.version",
+        "size": "metadata.total_size",
+        "files": "metadata.total_number_of_files",
+        "uuid": "metadata.uuid",
+        "created": "metadata.latest_write_timestamp",
+        "start_date": "metadata.start_date",
+        "end_date": "metadata.end_date",
+        "resolution": "metadata.resolution",
+        "frequency": "metadata.frequency",
+        "statistics_start_date": "metadata.statistics_start_date",
+        "statistics_end_date": "metadata.statistics_end_date",
+        "owner": "metadata.copyright",
+        "licence": "metadata.licence",
+    }
+
+    display.update(simple_mapping(record, mapping))
+
+    display.update(build_forcing_and_variables(record))
+
+    # statistics
+    display["statistics"] = ds.statistics
+    display["dtype"] = ds.dtype
+    display["chunks"] = ds.chunks
+
+    record = prepare_serialising_catalogue_record(record)
+
+    # check
+    _ = prepare_deserialisation_catalogue_record(record)
+    assert _["display"]["dtype"] == record["display"]["dtype"]
+    for k in record["display"]["statistics"]:
+        assert (_["display"]["statistics"][k] == record["display"]["statistics"][k]).all()
+
+    return record
+
+
+def prepare_serialising_catalogue_record(record):
+    record = deepcopy(record)
+
+    record["display"]["statistics"] = {k: v.tolist() for k, v in record["display"]["statistics"].items()}
+    record["display"]["dtype"] = str(record["display"]["dtype"])
+
+    return record
+
+
+def prepare_deserialisation_catalogue_record(record):
+    import numpy as np
+
+    record = deepcopy(record)
+
+    record["display"]["statistics"] = {k: np.array(v) for k, v in record["display"]["statistics"].items()}
+    record["display"]["dtype"] = np.dtype(record["display"]["dtype"])
+    return record
+
+
+if __name__ == "__main__":
+    import sys
+
+    print(create_catalogue_record(sys.argv[1]))
