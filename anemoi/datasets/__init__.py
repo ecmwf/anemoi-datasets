862091d03a5522460b6a7994af05b8dcb02db4be
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2024-04-29 09:16:55 +0000

    disable pre-commit

--- anemoi/datasets/__init__.py
+++ anemoi/datasets/__init__.py
@@ -10,7 +10,7 @@ from .data import add_dataset_path
 from .data import add_named_dataset
 from .data import open_dataset
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 __all__ = [
     "open_dataset",
