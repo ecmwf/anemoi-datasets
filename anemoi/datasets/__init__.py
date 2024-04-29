712533a291f9886f8df30e5b7840cb9e1d0940a4
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2024-04-29 09:09:46 +0000

    bump version

--- anemoi/datasets/__init__.py
+++ anemoi/datasets/__init__.py
@@ -10,7 +10,7 @@ from .data import add_dataset_path
 from .data import add_named_dataset
 from .data import open_dataset
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = [
     "open_dataset",
