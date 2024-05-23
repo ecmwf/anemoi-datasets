985cd0c97ee520e4663086d5fbaff6ed3dece88e
Florian Pinault <Florian.Pinault@ecmwf.int>
2024-05-23 09:17:03 +0000

    fix doc

--- docs/using/code/open_complex.py
+++ docs/using/code/open_complex.py
@@ -5,7 +5,7 @@ config = {
         "ensemble": [
             "/path/to/dataset1.zarr",
             {"dataset": "dataset_name", "end": 2010},
-            {"dataset": "s3://path/to/dataset3.zarr", "begin": 2000, "end": 2010},
+            {"dataset": "s3://path/to/dataset3.zarr", "start": 2000, "end": 2010},
         ],
         "frequency": "24h",
     },
