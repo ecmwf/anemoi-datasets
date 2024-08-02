b51c97849e8ce327f1e6951bc6c725cbfa440713
theissenhelen <helen.theissen@dtc.ox.ac.uk>
2024-08-02 16:44:16 +0100

    ci: skip test_create on self-hosted runner

--- tests/create/test_create.py
+++ tests/create/test_create.py
@@ -24,6 +24,7 @@ from anemoi.datasets.create import Creator
 from anemoi.datasets.data.stores import open_zarr
 
 IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
+SELF_HOSTED_RUNNER = os.getenv("RUNNER_TYPE") == "self-hosted"
 
 TEST_DATA_ROOT = "https://object-store.os-api.cci1.ecmwf.int/ml-tests/test-data/anemoi-datasets/create/"
 
@@ -211,7 +212,7 @@ class Comparer:
         compare_statistics(self.ds_output, self.ds_reference)
 
 
-@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
+@pytest.mark.skipif(IN_GITHUB_ACTIONS and SELF_HOSTED_RUNNER, reason="Test doesn't work in Github Actions.")
 @pytest.mark.parametrize("name", NAMES)
 @mockup_from_source
 def test_run(name):
