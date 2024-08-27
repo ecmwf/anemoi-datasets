fd4bb3414fb2eeac46669e4d8bd1195a829a8a72
b8raoult <53792887+b8raoult@users.noreply.github.com>
2024-08-27 14:39:20 +0100

    Move dates handling code to anemoi-utils

--- src/anemoi/datasets/dates/__init__.py
+++ src/anemoi/datasets/dates/__init__.py
@@ -7,124 +7,12 @@
 
 
 import datetime
-import re
 import warnings
 
-from anemoi.utils.dates import as_datetime
-
-
-def _compress_dates(dates):
-    dates = sorted(dates)
-    if len(dates) < 3:
-        yield dates
-        return
-
-    prev = first = dates.pop(0)
-    curr = dates.pop(0)
-    delta = curr - prev
-    while curr - prev == delta:
-        prev = curr
-        if not dates:
-            break
-        curr = dates.pop(0)
-
-    yield (first, prev, delta)
-    if dates:
-        yield from _compress_dates([curr] + dates)
-
-
-def compress_dates(dates):
-    dates = [as_datetime(_) for _ in dates]
-    result = []
-
-    for n in _compress_dates(dates):
-        if isinstance(n, list):
-            result.extend([str(_) for _ in n])
-        else:
-            result.append(" ".join([str(n[0]), "to", str(n[1]), "by", str(n[2])]))
-
-    return result
-
-
-def print_dates(dates):
-    print(compress_dates(dates))
-
-
-def no_time_zone(date):
-    return date.replace(tzinfo=None)
-
-
-def frequency_to_string(frequency):
-    # TODO: use iso8601
-    frequency = frequency_to_timedelta(frequency)
-
-    total_seconds = frequency.total_seconds()
-    assert int(total_seconds) == total_seconds, total_seconds
-    total_seconds = int(total_seconds)
-
-    seconds = total_seconds
-
-    days = seconds // (24 * 3600)
-    seconds %= 24 * 3600
-    hours = seconds // 3600
-    seconds %= 3600
-    minutes = seconds // 60
-    seconds %= 60
-
-    if days > 0 and hours == 0 and minutes == 0 and seconds == 0:
-        return f"{days}d"
-
-    if days == 0 and hours > 0 and minutes == 0 and seconds == 0:
-        return f"{hours}h"
-
-    if days == 0 and hours == 0 and minutes > 0 and seconds == 0:
-        return f"{minutes}m"
-
-    if days == 0 and hours == 0 and minutes == 0 and seconds > 0:
-        return f"{seconds}s"
-
-    if days > 0:
-        return f"{total_seconds}s"
-
-    return str(frequency)
-
-
-def frequency_to_timedelta(frequency):
-    # TODO: use iso8601 or check pytimeparse
-
-    if isinstance(frequency, datetime.timedelta):
-        return frequency
-
-    if isinstance(frequency, int):
-        return datetime.timedelta(hours=frequency)
-
-    assert isinstance(frequency, str), (type(frequency), frequency)
-
-    try:
-        return frequency_to_timedelta(int(frequency))
-    except ValueError:
-        pass
-
-    if re.match(r"^\d+[hdms]$", frequency, re.IGNORECASE):
-        unit = frequency[-1].lower()
-        v = int(frequency[:-1])
-        unit = {"h": "hours", "d": "days", "s": "seconds", "m": "minutes"}[unit]
-        return datetime.timedelta(**{unit: v})
-
-    m = frequency.split(":")
-    if len(m) == 2:
-        return datetime.timedelta(hours=int(m[0]), minutes=int(m[1]))
-
-    if len(m) == 3:
-        return datetime.timedelta(hours=int(m[0]), minutes=int(m[1]), seconds=int(m[2]))
-
-    raise ValueError(f"Cannot convert frequency {frequency} to timedelta")
-
-
-def normalize_date(x):
-    if isinstance(x, str):
-        return no_time_zone(datetime.datetime.fromisoformat(x))
-    return x
+# from anemoi.utils.dates import as_datetime
+from anemoi.utils.dates import frequency_to_timedelta
+from anemoi.utils.dates import normalize_date
+from anemoi.utils.dates import print_dates
 
 
 def extend(x):
@@ -203,7 +91,7 @@ class Dates:
 
 class ValuesDates(Dates):
     def __init__(self, values, **kwargs):
-        self.values = sorted([no_time_zone(_) for _ in values])
+        self.values = sorted([normalize_date(_) for _ in values])
         super().__init__(**kwargs)
 
     def __repr__(self):
@@ -232,8 +120,8 @@ class StartEndDates(Dates):
         if isinstance(end, datetime.date) and not isinstance(end, datetime.datetime):
             end = datetime.datetime(end.year, end.month, end.day)
 
-        start = no_time_zone(start)
-        end = no_time_zone(end)
+        start = normalize_date(start)
+        end = normalize_date(end)
 
         # if end <= start:
         #     raise ValueError(f"End date {end} must be after start date {start}")
