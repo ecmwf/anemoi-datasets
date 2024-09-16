696d9489f4ca3818ab19d4d6253c0f288da65a26
Michiel Van Ginderachter <michiel.vanginderachter@meteo.be>
2024-09-16 10:20:47 +0000

    add ECMWF copyright

--- src/anemoi/datasets/create/functions/filters/single_level_specific_humidity_to_relative_humidity.py
+++ src/anemoi/datasets/create/functions/filters/single_level_specific_humidity_to_relative_humidity.py
@@ -1,5 +1,11 @@
-# Written by Michiel Van Ginderachter (mpvginde)
-# Date: 2024-06-27
+# (C) Copyright 2024 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
 
 
 import numpy as np
