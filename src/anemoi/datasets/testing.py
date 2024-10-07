77504e7ea2a110a6e013574441d764fd2d2bf9ff
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2024-10-07 08:06:25 +0100

    update

--- src/anemoi/datasets/testing.py
+++ src/anemoi/datasets/testing.py
@@ -15,7 +15,12 @@ LOG = logging.getLogger(__name__)
 
 
 def assert_field_list(fs, size=None, start=None, end=None, constant=False, skip=None):
-    assert size is None or len(fs) == size, (len(fs), size)
+    import numpy as np
+
+    if size is None:
+        assert len(fs) > 0, fs
+    else:
+        assert len(fs) == size, (len(fs), size)
 
     first = fs[0]
     last = fs[-1]
@@ -40,3 +45,15 @@ def assert_field_list(fs, size=None, start=None, end=None, constant=False, skip=
     data = first.to_numpy(flatten=True)
 
     assert len(data) == len(latitudes), (len(data), len(latitudes))
+
+    north = np.max(latitudes)
+    south = np.min(latitudes)
+    east = np.max(longitudes)
+    west = np.min(longitudes)
+
+    assert north >= south, (north, south)
+    assert east >= west, (east, west)
+    assert north <= 90, north
+    assert south >= -90, south
+    assert east <= 360, east
+    assert west >= -180, west
