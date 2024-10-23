953f3825c8a24e7410ab2f4bbcc72f9e8ddd4f61
b8raoult <53792887+b8raoult@users.noreply.github.com>
2024-10-23 12:11:03 +0100

    Update copyright notice

--- tests/create/test_create.py
+++ tests/create/test_create.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python3
-# (C) Copyright 2023 European Centre for Medium-Range Weather Forecasts.
+# (C) Copyright 2024 Anemoi contributors.
+#
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
 # In applying this licence, ECMWF does not waive the privileges and immunities
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
+
 import glob
 import hashlib
 import json
