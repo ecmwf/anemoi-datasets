75e8c2eb7c4e83278013fc7caea29bea8bb12fe5
Florian Pinault <floriankrb@users.noreply.github.com>
2024-10-25 15:37:34 +0200

    fix tests (#97)
     fix tests
    
    Co-authored-by: Harrison Cook <harrison.cook@ecmwf.int>

--- src/anemoi/datasets/data/dataset.py
+++ src/anemoi/datasets/data/dataset.py
@@ -27,10 +27,10 @@ class Dataset:
     arguments = {}
 
     def mutate(self) -> "Dataset":
-        """
-        Give an opportunity to a subclass to return a new Dataset
+        """Give an opportunity to a subclass to return a new Dataset
         object of a different class, if needed.
         """
+
         return self
 
     def swap_with_parent(self, parent):
