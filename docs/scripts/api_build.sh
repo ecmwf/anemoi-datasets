cd3abe10918126d29885d8d0680397772f4ed320
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2025-03-29 08:51:02 +0000

    work on docs

--- docs/scripts/api_build.sh
+++ docs/scripts/api_build.sh
@@ -5,9 +5,9 @@ set -e
 script_dir=$(dirname "${BASH_SOURCE[0]}")
 docs_dir="$script_dir/.."
 source_dir="$script_dir/../../src/"
-cd "$source_dir"
 
-trap 'rm -f anemoi/__init__.py' EXIT
 
-touch anemoi/__init__.py
-sphinx-apidoc -M -f -o "$docs_dir/_api" anemoi -t "$docs_dir/_templates/apidoc"
+trap 'rm -f $source_dir/anemoi/__init__.py' EXIT
+
+touch "$source_dir/anemoi/__init__.py"
+sphinx-apidoc -M -f -o "$docs_dir/_api" "$source_dir/anemoi" -t "$docs_dir/_templates/apidoc"
