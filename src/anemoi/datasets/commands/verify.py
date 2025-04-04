dfc763b80f3cc8c8250f91fdf1233825ac6574f5
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2025-04-04 16:30:08 +0100

    feat: verify command

---
+++ src/anemoi/datasets/commands/verify.py
@@ -0,0 +1,43 @@
+# (C) Copyright 2024 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+import logging
+from typing import Any
+
+from . import Command
+
+LOG = logging.getLogger(__name__)
+
+
+class Verify(Command):
+    """Command to inspect a zarr dataset."""
+
+    def add_arguments(self, command_parser: Any) -> None:
+        """Add arguments to the command parser.
+
+        Parameters
+        ----------
+        command_parser : Any
+            The command parser.
+        """
+        command_parser.add_argument("path", metavar="DATASET")
+
+    def run(self, args: Any) -> None:
+        """Run the command.
+
+        Parameters
+        ----------
+        args : Any
+            The command arguments.
+        """
+        pass
+
+
+command = Verify
