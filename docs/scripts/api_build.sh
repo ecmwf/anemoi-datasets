ff974fdc61abbd60612f8f1d37337476a83fdb07
Ana Prieto Nemesio <ana.prietonemesio@ecmwf.int>
2025-04-25 16:10:56 +0200

    test

--- docs/scripts/api_build.sh
+++ docs/scripts/api_build.sh
@@ -1,18 +1,17 @@
 
+set -e
 
-# set -e
-
-# script_dir=$(dirname "${BASH_SOURCE[0]}")
-# docs_dir="$script_dir/.."
-# source_dir="$script_dir/../../src/"
-
-
-# trap 'rm -f $source_dir/anemoi/__init__.py' EXIT
-
-# touch "$source_dir/anemoi/__init__.py"
-# sphinx-apidoc -M -f -o "$docs_dir/_api" "$source_dir/anemoi" -t "$docs_dir/_templates/apidoc"
 script_dir=$(dirname "${BASH_SOURCE[0]}")
 docs_dir="$script_dir/.."
 source_dir="$script_dir/../../src/"
 
+
+trap 'rm -f $source_dir/anemoi/__init__.py' EXIT
+
+touch "$source_dir/anemoi/__init__.py"
 sphinx-apidoc -M -f -o "$docs_dir/_api" "$source_dir/anemoi" -t "$docs_dir/_templates/apidoc"
+# script_dir=$(dirname "${BASH_SOURCE[0]}")
+# docs_dir="$script_dir/.."
+# source_dir="$script_dir/../../src/"
+
+# sphinx-apidoc -M -f -o "$docs_dir/_api" "$source_dir/anemoi" -t "$docs_dir/_templates/apidoc"
