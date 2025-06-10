f32487646a8301c086cb4530709cd9b6fd5770a3
Vera Gahlen <vera.gahlen@ecmwf.int>
2025-06-10 16:09:51 +0000

    use fixtures in utils to get test data

---
+++ tests/conftest.py
@@ -0,0 +1,3 @@
+from anemoi.utils.testing import get_test_archive  # noqa: F401 [unused-import]
+from anemoi.utils.testing import get_test_data  # noqa: F401 [unused-import]
+from anemoi.utils.testing import temporary_directory_for_test_data  # noqa: F401 [unused-import]
