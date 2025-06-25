a22905f6c356e2c4d6eb2bbc7d3675f9ce1d84e4
Vera Gahlen <vera.gahlen@ecmwf.int>
2025-06-25 13:02:34 +0000

    register fixtures via pytest plugin

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,1 @@
-from anemoi.utils.testing import get_test_archive  # noqa: F401 [unused-import]
-from anemoi.utils.testing import get_test_data  # noqa: F401 [unused-import]
-from anemoi.utils.testing import temporary_directory_for_test_data  # noqa: F401 [unused-import]
+pytest_plugins = "anemoi.utils.testing"
