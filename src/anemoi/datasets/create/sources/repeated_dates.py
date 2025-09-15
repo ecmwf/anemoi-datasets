90e21eede3a3dcc833b566e4609c29e26dd0b95e
Matthew Chantry <matthew.chantry@ecmwf.int>
2025-09-15 16:06:00 +0100

    Fix copyright header

--- src/anemoi/datasets/create/sources/repeated_dates.py
+++ src/anemoi/datasets/create/sources/repeated_dates.py
@@ -22,16 +22,6 @@ from anemoi.utils.dates import frequency_to_timedelta
 from anemoi.datasets.create.source import Source
 from anemoi.datasets.create.sources import source_registry
 
-# (C) Copyright 2024 Anemoi contributors.
-#
-# This software is licensed under the terms of the Apache Licence Version 2.0
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
-#
-# In applying this licence, ECMWF does not waive the privileges and immunities
-# granted to it by virtue of its status as an intergovernmental organisation
-# nor does it submit to any jurisdiction.
-
-
 LOG = logging.getLogger(__name__)
 
 
