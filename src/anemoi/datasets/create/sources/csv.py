92c0023b29ac6d7069fb488630dc7e8c4508ce54
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2025-11-13 10:30:50 +0000

    update

--- src/anemoi/datasets/create/sources/csv.py
+++ src/anemoi/datasets/create/sources/csv.py
@@ -18,7 +18,15 @@ class CSVSource(ObservationsSource):
 
     emoji = "📄"  # For tracing
 
-    def __init__(self, context: any, path: str, *args: tuple, **kwargs: dict):
+    def __init__(
+        self,
+        context: any,
+        path: str,
+        columns: list = None,
+        flavour: dict = None,
+        *args,
+        **kwargs,
+    ):
         """Initialise the CSVSource.
 
         Parameters
@@ -27,16 +35,36 @@ class CSVSource(ObservationsSource):
             The context for the data source.
         filepath : str
             The path to the CSV file.
+        columns : list, optional
+            The list of columns to read from the CSV file.
         *args : tuple
             Additional positional arguments.
         **kwargs : dict
             Additional keyword arguments.
         """
         super().__init__(context, *args, **kwargs)
+
         self.path = path
+        self.columns = columns
+
+        self.flavour = {
+            "latitude": "latitude",
+            "longitude": "longitude",
+            "time": "time",
+        }
+
+        if flavour is not None:
+            self.flavour.update(flavour)
 
     def execute(self, dates):
         import pandas as pd
 
-        frame = pd.read_csv(self.path)
-        print(frame)
+        if self.columns is None:
+            frame = pd.read_csv(self.path)
+        else:
+            frame = pd.read_csv(self.path, usecols=self.columns)
+
+        start, end = dates.window.start_date, dates.window.end_date
+        mask = (frame[self.flavour["time"]] >= start) & (frame[self.flavour["time"]] <= end)
+        frame = frame.loc[mask]
+        return frame
