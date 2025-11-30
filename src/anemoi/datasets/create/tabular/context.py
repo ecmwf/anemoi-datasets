2f2a210115928d0805d40736ceedc8aba658729b
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2025-11-30 20:10:31 +0000

    update

--- src/anemoi/datasets/create/tabular/context.py
+++ src/anemoi/datasets/create/tabular/context.py
@@ -1,2 +1,22 @@
-class TabularContext:
-    pass
+from typing import Any
+
+from anemoi.datasets.create.input.context import Context
+
+
+class TabularContext(Context):
+    def __init__(self):
+        super().__init__()
+
+    def empty_result(self) -> Any:
+        import pandas as pd
+
+        return pd.DataFrame()
+
+    def source_argument(self, argument: Any) -> Any:
+        return argument  # .dates
+
+    def filter_argument(self, argument: Any) -> Any:
+        return argument
+
+    def create_result(self, argument, data):
+        raise NotImplementedError()
