99813376522adc7389a5742ff80d85525da224c9
SimonKamuk <skc@dmi.dk>
2025-12-01 17:01:32 +0000

    initial commit

--- src/anemoi/datasets/create/sources/xarray_support/patch.py
+++ src/anemoi/datasets/create/sources/xarray_support/patch.py
@@ -98,11 +98,83 @@ def patch_sort_coordinate(ds: xr.Dataset, sort_coordinates: list[str]) -> Any:
     return ds
 
 
+def patch_analysis_lead_to_valid_time(ds, time_coord_names) -> Any:
+    """Patch the dataset by converting analysis/lead time to valid time.
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        The dataset to patch.
+    time_coord_names : dict[str, str]
+        The names of the time coordinates. The keys must be:
+        analysis_time_coordinate : str
+            The name of the analysis time coordinate.
+        lead_time_coordinate : str
+            The name of the lead time coordinate.
+        valid_time_coordinate : str
+            The name of the valid time coordinate.
+
+    Returns
+    -------
+    Any
+        The patched dataset.
+    """
+
+    analysis_time_coordinate = time_coord_names["analysis_time_coordinate"]
+    lead_time_coordinate = time_coord_names["lead_time_coordinate"]
+    valid_time_coordinate = time_coord_names["valid_time_coordinate"]
+
+    valid_time = ds[analysis_time_coordinate] + ds[lead_time_coordinate]
+
+    ds = (
+        ds.assign_coords({valid_time_coordinate: valid_time})
+        .stack(time_index=[analysis_time_coordinate, lead_time_coordinate])
+        .set_index(time_index=valid_time_coordinate)
+        .rename(time_index=valid_time_coordinate)
+        .drop_vars([analysis_time_coordinate, lead_time_coordinate])
+    )
+
+    return ds
+
+
+def patch_rolling_sum(ds, vars_summation_period) -> Any:
+    """Patch the dataset by converting analysis/lead time to valid time.
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        The dataset to patch.
+    vars_summation_period: dict
+        The variables and summation period. The keys must be:
+        dim : str
+            The dimension to apply the rolling sum.
+        steps : int
+            The number of steps to sum over.
+        vars : list[str]
+            The variables to apply the rolling sum.
+
+    Returns
+    -------
+    Any
+        The patched dataset.
+    """
+    dim = vars_summation_period["dim"]
+    steps = vars_summation_period["steps"]
+    vars = vars_summation_period["vars"]
+
+    for var in vars:
+        ds[var] = ds[var].rolling(dim={dim: steps}, min_periods=steps).sum()
+
+    return ds
+
+
 PATCHES = {
     "attributes": patch_attributes,
     "coordinates": patch_coordinates,
     "rename": patch_rename,
     "sort_coordinates": patch_sort_coordinate,
+    "analysis_lead_to_valid_time": patch_analysis_lead_to_valid_time,
+    "rolling_sum": patch_rolling_sum,
 }
 
 
@@ -122,7 +194,7 @@ def patch_dataset(ds: xr.Dataset, patch: dict[str, dict[str, Any]]) -> Any:
         The patched dataset.
     """
 
-    ORDER = ["coordinates", "attributes", "rename", "sort_coordinates"]
+    ORDER = ["coordinates", "attributes", "rename", "sort_coordinates", "analysis_lead_to_valid_time", "rolling_sum"]
     for what, values in sorted(patch.items(), key=lambda x: ORDER.index(x[0])):
         if what not in PATCHES:
             raise ValueError(f"Unknown patch type {what!r}")
