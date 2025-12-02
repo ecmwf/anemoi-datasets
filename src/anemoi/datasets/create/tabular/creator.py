2a6573589587e192b2b5b5f247e5a6b3d2595d27
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2025-12-02 09:23:43 +0000

    update

--- src/anemoi/datasets/create/tabular/creator.py
+++ src/anemoi/datasets/create/tabular/creator.py
@@ -8,10 +8,11 @@ import rich
 
 from ..creator import Creator
 from ..locking import Locking
-from ..parts import PartFilter
 from .context import TabularContext
 
 LOG = logging.getLogger(__name__)
+
+VERSION = "0.30"
 
 
 class TabularDataset:
@@ -24,68 +25,6 @@ class TabularDataset:
 
 
 class TabularCreator(Creator):
-
-    def init(self):
-        rich.print(self.minimal_input)
-
-        lengths = tuple(len(g) for g in self.groups)
-        self.registry.create(lengths=lengths)
-
-    def load(self):
-        self.dataset = TabularDataset(self.path)
-        total = len(self.registry.get_flags())
-        self.chunk_filter = PartFilter(parts=self.parts, total=total)
-
-        self.data_array = self.dataset.data_array
-        self.n_groups = len(self.groups)
-        self.read_dataset_metadata(self.dataset.path)
-
-        for igroup, group in enumerate(self.groups):
-            if not self.chunk_filter(igroup):
-                continue
-            if self.registry.get_flag(igroup):
-                LOG.info(f" -> Skipping {igroup} total={len(self.groups)} (already done)")
-                continue
-
-            # assert isinstance(group[0], datetime.datetime), type(group[0])
-            LOG.debug(f"Building data for group {igroup}/{self.n_groups}")
-
-            result = self.input.select(self.context(), argument=group)
-            # assert result.group_of_dates == group, (len(result.group_of_dates), len(group), group)
-
-            # There are several groups.
-            # There is one result to load for each group.
-            self.load_result(result)
-            self.registry.set_flag(igroup)
-
-        self.registry.add_provenance(name="provenance_load")
-        self.tmp_statistics.add_provenance(name="provenance_load", config=self.main_config)
-
-        self.dataset.print_info()
-
-    def statistics(self):
-        rich.print("[red]Statistics method not implemented yet.[/red]")
-
-    def size(self):
-        rich.print("[red]Size method not implemented yet.[/red]")
-
-    def cleanup(self):
-        rich.print("[red]Cleanup method not implemented yet.[/red]")
-
-    def init_additions(self):
-        rich.print("[red]Init additions method not implemented yet.[/red]")
-
-    def load_additions(self):
-        rich.print("[red]Load additions method not implemented yet.[/red]")
-
-    def finalise_additions(self):
-        rich.print("[red]Finalise additions method not implemented yet.[/red]")
-
-    def patch(self):
-        rich.print("[red]Patch method not implemented yet.[/red]")
-
-    def verify(self):
-        rich.print("[red]Verify method not implemented yet.[/red]")
 
     ######################################################
 
