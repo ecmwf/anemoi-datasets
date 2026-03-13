ecmwf/anemoi-datasets

commit c5408dafd34c80bcb4b0cd9623dbebfe00959151

feat: refactor dataset create (#379)
## Description

This is a rewrite of the code that parses the recipe and invoke the
sources and filters in the right order. These changes are merely a
simplification of the old version and are 100% backward compatible.
The input family of actions has been refactored such that all the
standalone action files, e.g. concat are consolidated into a single
action.py.

Additional related additions.
- Add facility to have a source as a parameter of an other source if
needed.
- Changes are due to code moving to their own file (i.e repeated_dates).
- A new CLI tool anemoi-datasets recipe was added to check the change.
It can be use to format the YAML and validate it (against a simple JSON
schema), and migrate older YAML files to new syntax. The later is
experimental, and as been tested with very old recipe files. It can be a
good starting point for future migrations, if ever needed.


## What problem does this change solve?

This change is in preparation of adding support for observation in
anemoi-dataset[create].

## What issue or task does this change relate to?
<!-- link to Issue Number -->

##  Additional notes ##
<!-- Include any additional information, caveats, or considerations that
the reviewer should be aware of. -->

***As a contributor to the Anemoi framework, please ensure that your
changes include unit tests, updates to any affected dependencies and
documentation, and have been tested in a parallel setting (i.e., with
multiple GPUs). As a reviewer, you are also responsible for verifying
these aspects and requesting changes if they are not adequately
addressed. For guidelines about those please refer to
https://anemoi.readthedocs.io/en/latest/***

By opening this pull request, I affirm that all authors agree to the
[Contributor License
Agreement.](https://github.com/ecmwf/codex/blob/main/Legal/contributor_license_agreement.md)

---------

Co-authored-by: Florian Pinault <floriankrb@users.noreply.github.com>

src/anemoi/datasets/create/sources/repeated_dates.py
@@ -19,13 +19,18 @@ from anemoi.transform.fields import new_fieldlist_from_list
 from anemoi.utils.dates import as_datetime
 from anemoi.utils.dates import frequency_to_timedelta
 
-from .action import Action
-from .action import action_factory
-from .join import JoinResult
-from .result.field import Result
-from .trace import trace_select
+from anemoi.datasets.create.source import Source
+from anemoi.datasets.create.sources import source_registry
 
 LOG = logging.getLogger(__name__)
+
+
+class Action:
+    pass
+
+
+class Result:
+    pass
 
 
 class DateMapper:
@@ -278,109 +283,24 @@ class DateMapperConstant(DateMapper):
         ]
 
 
-class DateMapperResult(Result):
-    """A Result implementation that updates the valid datetime of the datasource."""
+@source_registry.register("repeated_dates")
+class RepeatedDatesSource(Source):
 
-    def __init__(
-        self,
-        context: Any,
-        action_path: list[str],
-        group_of_dates: Any,
-        source_result: Any,
-        mapper: DateMapper,
-        original_group_of_dates: Any,
-    ) -> None:
-        """Initialize DateMapperResult.
+    def __init__(self, context, source: Any, mode: str, **kwargs) -> None:
+        # assert False, (context, source, mode, kwargs)
+        super().__init__(context, **kwargs)
+        self.mapper = DateMapper.from_mode(mode, source, kwargs)
+        self.source = source
 
-        Parameters
-        ----------
-        context : Any
-            The context.
-        action_path : list of str
-            The action path.
-        group_of_dates : Any
-            The group of dates.
-        source_result : Any
-            The source result.
-        mapper : DateMapper
-            The date mapper.
-        original_group_of_dates : Any
-            The original group of dates.
-        """
-        super().__init__(context, action_path, group_of_dates)
+    def execute(self, group_of_dates):
+        source = self.context.create_source(self.source, "data_sources", str(id(self)))
 
-        self.source_results: Any = source_result
-        self.mapper: DateMapper = mapper
-        self.original_group_of_dates: Any = original_group_of_dates
-
-    @property
-    def datasource(self) -> Any:
-        """Get the datasource with updated valid datetime."""
-        result: list = []
-
-        for field in self.source_results.datasource:
-            for date in self.original_group_of_dates:
-                result.append(new_field_with_valid_datetime(field, date))
-
-        if not result:
-            raise ValueError("repeated_dates: no input data found")
+        result = []
+        for one_date_group, many_dates_group in self.mapper.transform(group_of_dates):
+            print(f"one_date_group: {one_date_group}, many_dates_group: {many_dates_group}")
+            source_results = source(self.context, one_date_group)
+            for field in source_results:
+                for date in many_dates_group:
+                    result.append(new_field_with_valid_datetime(field, date))
 
         return new_fieldlist_from_list(result)
-
-
-class RepeatedDatesAction(Action):
-    """An Action implementation that selects and transforms a group of dates."""
-
-    def __init__(self, context: Any, action_path: list[str], source: Any, mode: str, **kwargs: Any) -> None:
-        """Initialize RepeatedDatesAction.
-
-        Args:
-            context (Any): The context.
-            action_path (List[str]): The action path.
-            source (Any): The data source.
-            mode (str): The mode for date mapping.
-            **kwargs (Any): Additional arguments.
-        """
-        super().__init__(context, action_path, source, mode, **kwargs)
-
-        self.source: Any = action_factory(source, context, action_path + ["source"])
-        self.mapper: DateMapper = DateMapper.from_mode(mode, self.source, kwargs)
-        self.mode = mode
-        self.kwargs = kwargs
-
-    @trace_select
-    def select(self, group_of_dates: Any) -> JoinResult:
-        """Select and transform the group of dates.
-
-        Args:
-            group_of_dates (Any): The group of dates to select.
-
-        Returns
-        -------
-        JoinResult
-            The result of the join operation.
-        """
-        results: list = []
-        for one_date_group, many_dates_group in self.mapper.transform(group_of_dates):
-            results.append(
-                DateMapperResult(
-                    self.context,
-                    self.action_path,
-                    one_date_group,
-                    self.source.select(one_date_group),
-                    self.mapper,
-                    many_dates_group,
-                )
-            )
-
-        return JoinResult(self.context, self.action_path, group_of_dates, results)
-
-    def __repr__(self) -> str:
-        """Get the string representation of the action.
-
-        Returns
-        -------
-        str
-            The string representation.
-        """
-        return f"MultiDateMatchAction({self.source}, {self.mapper})"

.gitignore
@@ -141,3 +141,13 @@ _version.py
 *.to_upload
 tempCodeRunnerFile.python
 Untitled-*.py
+*.zip
+*.json
+*.db
+*.tgz
+_api/
+trace.txt
+?/
+*.prof
+prof/
+*.gz

pyproject.toml
@@ -51,9 +51,12 @@ dependencies = [
   "anemoi-transform>=0.1.10",
   "anemoi-utils[provenance]>=0.4.32",
   "cfunits",
+  "glom",
+  "jsonschema",
   "numcodecs<0.16",                   # Until we move to zarr3
   "numpy",
   "pyyaml",
+  "ruamel-yaml",
   "semantic-version",
   "tqdm",
   "zarr<=2.18.4",

src/anemoi/datasets/commands/recipe/__init__.py
@@ -1,0 +1,93 @@
+# (C) Copyright 2024 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+import argparse
+import logging
+import sys
+from typing import Any
+
+import yaml
+
+from anemoi.datasets.create import validate_config
+
+from .. import Command
+from .format import format_recipe
+from .migrate import migrate_recipe
+
+LOG = logging.getLogger(__name__)
+
+
+class Recipe(Command):
+    def add_arguments(self, command_parser: Any) -> None:
+        """Add arguments to the command parser.
+
+        Parameters
+        ----------
+        command_parser : Any
+            Command parser object.
+        """
+
+        command_parser.add_argument("--validate", action="store_true", help="Validate recipe.")
+        command_parser.add_argument("--format", action="store_true", help="Format the recipe.")
+        command_parser.add_argument("--migrate", action="store_true", help="Migrate the recipe to the latest version.")
+
+        group = command_parser.add_mutually_exclusive_group()
+        group.add_argument("--inplace", action="store_true", help="Overwrite the recipe file in place.")
+        group.add_argument("--output", type=str, help="Output file path for the converted recipe.")
+
+        command_parser.add_argument(
+            "path",
+            help="Path to recipe.",
+        )
+
+    def run(self, args: Any) -> None:
+
+        if not args.validate and not args.format and not args.migrate:
+            args.validate = True
+
+        with open(args.path) as file:
+            config = yaml.safe_load(file)
+
+        assert isinstance(config, dict)
+
+        if args.validate:
+            if args.inplace and (not args.format and not args.migrate):
+                argparse.ArgumentError(None, "--inplace is not supported with --validate.")
+
+            if args.output and (not args.format and not args.migrate):
+                argparse.ArgumentError(None, "--output is not supported with --validate.")
+
+            validate_config(config)
+            LOG.info(f"{args.path}: Recipe is valid.")
+            return
+
+        if args.migrate:
+            config = migrate_recipe(args, config)
+            if config is None:
+                LOG.info(f"{args.path}: No changes needed.")
+                return
+
+            args.format = True
+
+        if args.format:
+            formatted = format_recipe(args, config)
+            assert "dates" in formatted
+            f = sys.stdout
+            if args.output:
+                f = open(args.output, "w")
+
+            if args.inplace:
+                f = open(args.path, "w")
+
+            print(formatted, file=f)
+            f.close()
+
+
+command = Recipe

src/anemoi/datasets/commands/recipe/format.py
@@ -1,0 +1,55 @@
+# (C) Copyright 2025 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+import datetime
+import logging
+
+from ...dumper import yaml_dump
+
+LOG = logging.getLogger(__name__)
+
+
+def make_dates(config):
+    if isinstance(config, dict):
+        return {k: make_dates(v) for k, v in config.items()}
+    if isinstance(config, list):
+        return [make_dates(v) for v in config]
+    if isinstance(config, str):
+        try:
+            return datetime.datetime.fromisoformat(config)
+        except ValueError:
+            return config
+    return config
+
+
+ORDER = (
+    "name",
+    "description",
+    "dataset_status",
+    "licence",
+    "attribution",
+    "env",
+    "dates",
+    "common",
+    "data_sources",
+    "input",
+    "output",
+    "statistics",
+    "build",
+    "platform",
+)
+
+
+def format_recipe(args, config: dict) -> str:
+
+    config = make_dates(config)
+    assert config
+
+    return yaml_dump(config, order=ORDER)

commit b73d533ee5dd351e8cc390255eff6642b497823f

fix(create): repeated-dates (#572)
## Description
Fixes issue #566 

## What problem does this change solve?
The `closest` mode of the `repeat-dates` source has been broken since
the #379 refactor.
This PR fixes that by correctly applying the refactors to the
`DateMapper` classes.

***As a contributor to the Anemoi framework, please ensure that your
changes include unit tests, updates to any affected dependencies and
documentation, and have been tested in a parallel setting (i.e., with
multiple GPUs). As a reviewer, you are also responsible for verifying
these aspects and requesting changes if they are not adequately
addressed. For guidelines about those please refer to
https://anemoi.readthedocs.io/en/latest/***

By opening this pull request, I affirm that all authors agree to the
[Contributor License
Agreement.](https://github.com/ecmwf/codex/blob/main/Legal/contributor_license_agreement.md)

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: Florian Pinault <floriankrb@users.noreply.github.com>

src/anemoi/datasets/create/input/repeated_dates.py
@@ -77,11 +77,13 @@ class DateMapperClosest(DateMapper):
         self.tried: set[Any] = set()
         self.found: set[Any] = set()
 
-    def transform(self, group_of_dates: Any) -> Generator[tuple[Any, Any], None, None]:
+    def transform(self, context, group_of_dates: Any) -> Generator[tuple[Any, Any], None, None]:
         """Transform the group of dates to the closest available dates.
 
         Parameters
         ----------
+        context:
+            The context of the RepeatedSource using the DateMapper
         group_of_dates : Any
             The group of dates to transform.
 
@@ -116,16 +118,17 @@ class DateMapperClosest(DateMapper):
             # return []
 
         if to_try:
-            result = self.source.select(
+            result = self.source(
+                context,
                 GroupOfDates(
                     sorted(to_try),
                     group_of_dates.provider,
                     partial_ok=True,
-                )
+                ),
             )
 
             cnt = 0
-            for f in result.datasource:
+            for f in result:
                 cnt += 1
                 # We could keep the fields in a dictionary, but we don't want to keep the fields in memory
                 date = as_datetime(f.metadata("valid_datetime"))
@@ -190,11 +193,13 @@ class DateMapperClimatology(DateMapper):
         self.day: int = day
         self.hour: int | None = hour
 
-    def transform(self, group_of_dates: Any) -> Generator[tuple[Any, Any], None, None]:
+    def transform(self, context, group_of_dates: Any) -> Generator[tuple[Any, Any], None, None]:
         """Transform the group of dates to the specified climatology dates.
 
         Parameters
         ----------
+        context:
+            The context of the RepeatedSource using the DateMapper (not used here)
         group_of_dates : Any
             The group of dates to transform.
 
@@ -239,11 +244,13 @@ class DateMapperConstant(DateMapper):
         self.source: Any = source
         self.date: Any | None = date
 
-    def transform(self, group_of_dates: Any) -> tuple[Any, Any]:
+    def transform(self, context, group_of_dates: Any) -> tuple[Any, Any]:
         """Transform the group of dates to a constant date.
 
         Parameters
         ----------
+        context:
+            The context of the RepeatedSource using the DateMapper (not used here)
         group_of_dates : Any
             The group of dates to transform.
 

src/anemoi/datasets/create/sources/repeated_dates.py
@@ -27,16 +27,18 @@ class RepeatedDatesSource(Source):
     def __init__(self, context, source: Any, mode: str, **kwargs) -> None:
         # assert False, (context, source, mode, kwargs)
         super().__init__(context, **kwargs)
+
+        source = self.context.create_source(source, "data_sources", str(id(self)))
+
         self.mapper = DateMapper.from_mode(mode, source, kwargs)
         self.source = source
 
     def execute(self, group_of_dates):
-        source = self.context.create_source(self.source, "data_sources", str(id(self)))
 
         result = []
-        for one_date_group, many_dates_group in self.mapper.transform(group_of_dates):
+        for one_date_group, many_dates_group in self.mapper.transform(self.context, group_of_dates):
             print(f"one_date_group: {one_date_group}, many_dates_group: {many_dates_group}")
-            source_results = source(self.context, one_date_group)
+            source_results = self.source(self.context, one_date_group)
             for field in source_results:
                 for date in many_dates_group:
                     result.append(new_field_with_valid_datetime(field, date))