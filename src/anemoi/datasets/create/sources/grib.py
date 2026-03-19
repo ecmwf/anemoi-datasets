1d6bea87ee8defb6f51c18e6917ed756ddab2140
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2026-03-19 13:24:01 +0000

    revert ecfs code

--- src/anemoi/datasets/create/sources/grib.py
+++ src/anemoi/datasets/create/sources/grib.py
@@ -120,7 +120,6 @@ class GribSource(LegacySource):
             The loaded dataset.
         """
         given_paths = path if isinstance(path, list) else [path]
-
         if flavour is not None:
             flavour = RuleBasedFlavour(flavour)
 
@@ -146,12 +145,6 @@ class GribSource(LegacySource):
 
             for path in _expand(paths):
                 context.trace("📁", "PATH", path)
-
-            if isinstance(path, str) and (path.startswith("ec:") or path.startswith("ectmp:")):
-                from anemoi.datasets.create.ecfs import get_ecfs_file
-
-                path = get_ecfs_file(path)
-
                 s = from_source("file", path)
                 if flavour is not None:
                     s = flavour.map(s)
--- src/anemoi/datasets/create/sources/xarray_support/__init__.py
+++ src/anemoi/datasets/create/sources/xarray_support/__init__.py
@@ -96,11 +96,6 @@ def load_one(
 
     context.trace(emoji, dataset, options, kwargs)
 
-    if isinstance(dataset, str) and (dataset.startswith("ec:") or dataset.startswith("ectmp:")):
-        from anemoi.datasets.create.ecfs import get_ecfs_file
-
-        dataset = get_ecfs_file(dataset)
-
     if isinstance(dataset, str) and dataset.endswith(".zarr"):
         # If the dataset is a zarr store, we need to use the zarr engine
         options["engine"] = "zarr"
