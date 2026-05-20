7f176d227979514392141a189291b8e0a6b8c967
Neill Bowler <neill.bowler@metoffice.gov.uk>
2026-05-20 09:14:17 +0100

    Move validation to build class for pydantic.

--- src/anemoi/datasets/create/tabular/creator.py
+++ src/anemoi/datasets/create/tabular/creator.py
@@ -59,20 +59,6 @@ class TabularCreator(Creator):
         dataset : Dataset
             The dataset object to be initialised with arrays and coordinates.
         """
-        # Check the recipt for mandatory fields in tabular datasets
-        if hasattr(self, "recipe"):
-            if hasattr(self.recipe, "build"):
-                if not hasattr(self.recipe.build, "max_fragment_size"):
-                    LOG.info("self.recipe.build.max_fragment_size was not specified, "
-                             "defaulting to 268435456 (256MiB)")
-                    setattr(self.recipe.build, "max_fragment_size", 268435456)
-                if not hasattr(self.recipe.build, "validate_date_ranges"):
-                    LOG.info("self.recipe.build.validate_date_ranges was not specified, "
-                             "defaulting to true")
-                    setattr(self.recipe.build, "validate_date_ranges", True)
-                if not hasattr(self.recipe.build, "max_workers"):
-                    LOG.info("self.recipe.build.max_workers was not specified, " "defaulting to 1")
-                    setattr(self.recipe.build, "max_workers", 1)
         pass
 
     ######################################################
