4e384e5763b77e62206589c753bdd0d841757246
Natalie Zelenka <natalie.zelenka@ecmwf.int>
2026-05-27 15:14:47 +0100

    Added an expected missing date

--- tests/create/test_missing_range.py
+++ tests/create/test_missing_range.py
@@ -48,6 +48,7 @@ def test_missing_range_dates_are_supported(tmp_path: Path, load_source: LoadSour
         np.datetime64("2021-01-01T12:00:00"),
         np.datetime64("2021-01-02T00:00:00"),
         np.datetime64("2021-01-02T12:00:00"),
+        np.datetime64("2021-01-03T12:00:00"),
     ]
 
 
@@ -72,4 +73,5 @@ def test_missing_mixture_dates_are_supported(tmp_path: Path, load_source: LoadSo
         np.datetime64("2021-01-01T12:00:00"),
         np.datetime64("2021-01-02T00:00:00"),
         np.datetime64("2021-01-02T12:00:00"),
+        np.datetime64("2021-01-03T12:00:00"),
     ]
