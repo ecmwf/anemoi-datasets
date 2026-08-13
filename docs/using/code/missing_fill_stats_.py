e127d7f342a05ada2795666e9af3ae16c7fccac0
b8raoult <53792887+b8raoult@users.noreply.github.com>
2026-08-13 13:39:21 +0200

    docs: update usage doc (#712)
    ## Description
    
    Some tiding up of the docs, mostly to move example of code in their own
    file so they are considered by the pre-commit hooks (black, ruff, etc).
    The examples will be formatted in a consistent manner, using the same
    options as the main code.
    
    In addition, a page explaining a bit more of the philosophy behind that
    package, called "how it works", visible here:
    https://anemoi--712.org.readthedocs.build/projects/datasets/en/712/using/how-it-works.html.
    
    ## What problem does this change solve?
    <!-- Describe if it's a bugfix, new feature, doc update, or breaking
    change -->
    
    ## What issue or task does this change relate to?
    <!-- link to Issue Number -->
    
    ##  Additional notes ##
    <!-- Include any additional information, caveats, or considerations that
    the reviewer should be aware of. -->
    
    ***As a contributor to the Anemoi framework, please ensure that your
    changes include unit tests, updates to any affected dependencies and
    documentation, and have been tested in a parallel setting (i.e., with
    multiple GPUs). As a reviewer, you are also responsible for verifying
    these aspects and requesting changes if they are not adequately
    addressed. For guidelines about those please refer to
    https://anemoi.readthedocs.io/en/latest/***
    
    By opening this pull request, I affirm that all authors agree to the
    [Contributor License
    Agreement.](https://github.com/ecmwf/codex/blob/main/Legal/Contributor-License-Agreement.md)
    
    
    <!-- readthedocs-preview anemoi-datasets start -->
    ----
    📚 Documentation preview 📚:
    https://anemoi-datasets--712.org.readthedocs.build/en/712/
    
    <!-- readthedocs-preview anemoi-datasets end -->

---
+++ docs/using/code/missing_fill_stats_.py
@@ -0,0 +1,3 @@
+ds = open_dataset(dataset, fill_missing_dates="mean")
+ds = open_dataset(dataset, fill_missing_dates="maximum")
+ds = open_dataset(dataset, fill_missing_dates="minimum")
---
+++ docs/using/code/opening_url.py
@@ -0,0 +1,5 @@
+from anemoi.datasets import open_dataset
+
+ds1 = open_dataset("https://path/to/dataset.zarr")
+
+ds2 = open_dataset("s3://path/to/dataset.zarr")
